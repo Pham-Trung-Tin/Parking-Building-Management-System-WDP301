090adf6cc05888aa0c47f1269a759c3872cb1809
Tin <tinptse182207@fpt.edu.vn>
2026-05-19 12:42:58 +0700

    update v2

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>banh mi nong hoi 2</h1>
+          <h1>banh mi nong hoi 3</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
