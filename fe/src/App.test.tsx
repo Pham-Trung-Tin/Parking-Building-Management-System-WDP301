d14cfcca96b60e06f391f430161f069ef7747daf
Tin <tinptse182207@fpt.edu.vn>
2026-05-19 12:57:45 +0700

    Unit test

---
+++ fe/src/App.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import App from './App';
+import React from 'react';
+
+test('Kiểm tra nút count tăng số khi click', async () => {
+  render(<App />);
+  
+  // 1. Tìm cái nút có chữ "Count is 0"
+  const button = screen.getByRole('button');
+  expect(button.innerHTML).toBe('Count is 0');
+
+  // 2. Mô phỏng hành động click chuột vào nút
+  fireEvent.click(button);
+
+  // 3. Kiểm tra xem nó có tăng lên 1 chưa
+  expect(button.innerHTML).toBe('Count is 1');
+});
